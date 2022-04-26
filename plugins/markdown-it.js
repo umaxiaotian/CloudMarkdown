e5a6fa7de39d6a061fcab829dbc05dd60773f315
小畑佑馬 <yuumaobata1008@outlook.com>
2022-04-27 06:02:05 +0900

    no message

--- plugins/markdown-it.js
+++ plugins/markdown-it.js
@@ -1,6 +1,6 @@
 import MarkdownIt from 'markdown-it'
 import markdownItGithubToc from 'markdown-it-github-toc'
-import MarkdownItMermaid from '@datatracCorporation/markdown-it-mermaid'
+import MarkdownItMermaid from '@DatatracCorporation/markdown-it-mermaid'
 export default ({ app }, inject) => {
   const md = new MarkdownIt({
     langPrefix: 'language-',
