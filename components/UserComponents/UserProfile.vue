73cd47a8baac81a986506c3eb69bd67b8d6cbc80
小畑佑馬 <yuumaobata1008@outlook.com>
2022-05-14 17:46:40 +0900

    APIからデータを取得できるように修正

--- components/UserComponents/UserProfile.vue
+++ components/UserComponents/UserProfile.vue
@@ -27,22 +27,22 @@
                 <v-row>
                   <v-col cols="2">
                     <v-list-item-avatar>
-                      <v-img :src="'default.jpg' | changeImgPath"></v-img>
+                      <v-img :src="user.avater | changeImgPath"></v-img>
                     </v-list-item-avatar>
                   </v-col>
                   <v-col cols="10">
-                    <v-file-input :rules="rules" accept="image/png, image/jpeg, image/bmp" placeholder="画像を挿入してください"
-                      prepend-icon="mdi-plus-box-multiple" label="プロフィール画像"></v-file-input>
+                    <v-file-input :value="user.avater | profileImgToText" accept="image/png, image/jpeg, image/bmp" placeholder="画像を挿入してください"
+                      prepend-icon="mdi-plus-box-multiple" label="プロフィール画像" ></v-file-input>
                   </v-col>
                   <v-col cols="12" sm="6" md="6">
-                    <v-text-field label="氏名*" value="テスト" required></v-text-field>
+                    <v-text-field label="氏名*"  v-model="user.name" required></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="6">
-                    <v-text-field label="ニックネーム*" hint="このウェブサイトでユーザー名として表示されます。" persistent-hint required>
+                    <v-text-field label="ニックネーム*"   v-model="user.nickname"  hint="このウェブサイトでユーザー名として表示されます。" persistent-hint required>
                     </v-text-field>
                   </v-col>
                   <v-col cols="12">
-                    <v-text-field label="メールアドレス*" required></v-text-field>
+                    <v-text-field label="メールアドレス*"  v-model="user.email" required></v-text-field>
                   </v-col>
                 </v-row>
               </v-container>
@@ -53,7 +53,7 @@
               <v-btn color="blue darken-1" text @click="_model = false">
                 閉じる
               </v-btn>
-              <v-btn color="blue darken-1" text @click="_model = false"> 保存 </v-btn>
+              <v-btn color="blue darken-1" text @click="saveUserProfile()"> 保存 </v-btn>
             </v-card-actions>
           </v-card>
         </v-tab-item>
@@ -95,27 +95,29 @@
 export default {
   data() {
     return {
-      dialog: true,
+      user: [],
     };
   },
   model: {
-    prop: 'model',
-    event: 'change'
+    prop: 'parentModel',
+    event: 'input'
   },
   props: {
-    model: {
-      type: String,
-      default: ''
-    }
+    parentModel: Boolean
   },
   computed: {
     _model: {
-      get() {
-        return this.model
+      get () {
+        return this.parentModel
       },
-      set(value) {
-        this.$emit('change', value)
+      set (value) {
+        this.$emit('input', value)
       }
+    }
+  },
+  methods:{
+    saveUserProfile(){
+      console.log(this.user.name);
     }
   },
   filters: {
@@ -123,7 +125,19 @@ export default {
       var BaseUrl = process.env.baseUrl;
       return BaseUrl + "/extraResource/" + img;
     },
+    profileImgToText(value){
+      return new File(["image"], value, {
+        type: "text/plain",
+      });
+    }
   },
-  async created() { },
+  async created() { 
+    const isLogin = await this.$util.isLogin();
+    if (isLogin) {
+      const acsessToken = this.$store.getters.accessToken;
+      const header = { Authorization: "Bearer " + acsessToken };
+      this.user = await this.$api.apiGet("/user/me/", header);
+    }
+  },
 };
 </script>
